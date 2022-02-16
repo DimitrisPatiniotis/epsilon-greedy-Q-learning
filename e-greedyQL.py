d0e24f65dae8bc3fe71a6fcd36db066b063fa89f
DimitrisPatiniotis <dimpatspy@gmail.com>
2022-02-16 23:22:16 +0200

    re-assigning wrong moves

--- e-greedyQL.py
+++ e-greedyQL.py
@@ -1,8 +1,12 @@
 import numpy as np
 import random
+from time import sleep
+import matplotlib.pyplot as plt
 
 epsilon = 1
 decay = 0.01
+learning_rate = 0.05
+gamma = 0.9
 
 capacity = [
     [2.5, 2.5],
@@ -14,7 +18,7 @@ actions = [
     [1,0] , [1,1]
 ]
 
-Q_table = [0, 0, 0, 0]
+Q_table = [1, 1, 1, 1]
 
 class Agent:
     
@@ -24,11 +28,19 @@ class Agent:
         self.epsilon = epsilon
         self.q_table = Q_table
         self.actions = actions
+        self.decay = decay
         self.move = None
         self.reward = None
-    
+        self.total_reward = 0
+
+    def reduce_epsilon(self):
+        if self.epsilon >= 0.01:
+            self.epsilon = self.epsilon - self.decay
+
     def select_move(self):
-        if np.random.rand() <= self.epsilon:
+        if self.epsilon <= 0:
+            return np.argmax(self.q_table)
+        elif np.random.rand() <= self.epsilon:
             return random.randrange(0, len(self.q_table))
         else:
             return np.argmax(self.q_table)
@@ -40,9 +52,14 @@ class Agent:
     
     def get_reward(self, reward):
         self.reward = reward
+        self.total_reward += reward
+        print(self.total_reward)
 
     def update_q(self):
-        pass
+        index = self.actions.index(self.move)
+        self.q_table[index] = self.q_table[index] + learning_rate * (self.reward + (gamma * np.max(self.q_table)) - self.q_table[index])
+
+
 
 def generate_agents(number_weight_tuple_list):
     agent_num = 1
@@ -72,18 +89,61 @@ def distribute_reward(agents, state):
             reward[i][x] = round((2.5 - state[i][x]),1)
     for i in agents:
         i.reward = reward[i.move[0]][i.move[1]]
+        i.total_reward += reward[i.move[0]][i.move[1]]
+
 
 def update_q_values(agents):
     for i in agents: i.update_q()
 
+def test_overweight(state):
+    if state[0][0] > 2.5:
+        return [0, 0]
+    elif state[0][1] > 2.5:
+        return [0,1]
+    elif state[1][0] > 2.5:
+        return [1,0]
+    elif state[1][1] > 2.5:
+        return [1,1]
+    else:
+        return False
+
+def get_avg_total_r(agents):
+    return sum([i.total_reward for i in agents])
 
 def main():
     agents, len_agents = generate_agents([(2,0.6), (5, 0.4), (8, 0.2)])
-    agent_weights = [f.weight for f in agents]
-    for i in agents: i.make_move()
-    turn_state = calculate_total_distribution(agents)
-    distribute_reward(agents, turn_state)
-    update_q_values(agents)
+    total_mistakes = 0
+    mistakes_per_turn = []
+    avg_rewards = []
+    for turn in range(1,2500):
+        for i in agents: i.make_move()
+        turn_state = calculate_total_distribution(agents)
+        
+        time = 0
+
+        while test_overweight(turn_state) != False:
+            total_mistakes += 1
+            for i in agents:
+                if i.move == test_overweight(turn_state):
+                    distribute_reward([i], turn_state)
+                    update_q_values([i])
+                    i.make_move()
+            turn_state = calculate_total_distribution(agents)
+        mistakes_per_turn.append(round(total_mistakes/turn, 3))
+        avg_rewards.append(get_avg_total_r(agents))
+        distribute_reward(agents, turn_state)
+        update_q_values(agents)
+        if turn%20 == 0:
+            for i in agents:
+                i.reduce_epsilon()
+
+    fig, ax = plt.subplots()
+    ax.plot(range(1,2500), mistakes_per_turn)
+    ax.set(xlabel='time (s)', ylabel='mistakes', title='Avarage Mistakes Per Turn')
+    ax.grid()
+    plt.show()
+
+
 
 if __name__ == '__main__':
     main()
